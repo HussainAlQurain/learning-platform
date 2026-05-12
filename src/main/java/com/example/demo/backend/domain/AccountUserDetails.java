04e3367f13551bba3d48de232f7e0cd22c81f9b3
Hussain <g202316090@kfupm.edu.sa>
2026-05-13 00:56:37 +0300

    create class that implements userdetail class to load users

---
+++ src/main/java/com/example/demo/backend/domain/AccountUserDetails.java
@@ -0,0 +1,61 @@
+package com.example.demo.backend.domain;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AccountUserDetails implements UserDetails {
+
+    private final Account account;
+
+    public AccountUserDetails(Account account) {
+        this.account = account;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public Long getId() {
+        return account.getId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return account.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getUsername();
+    }
+}
---
+++ src/main/java/com/example/demo/backend/service/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.demo.backend.service;
+
+import com.example.demo.backend.domain.AccountUserDetails;
+import com.example.demo.backend.repo.AccountRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    public MyUserDetailsService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.accountRepository.findAccountByUsername(username)
+                .map(AccountUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User %s not found", username)));
+    }
+}
